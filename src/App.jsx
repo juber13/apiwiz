dc9f01fbe7caf4d4362f744267c3a09c24e781eb
juber <juberk1010@gmail.com>
2024-04-20 14:35:36 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Content from "./components/Content"
 function App() {
   return (
     <div className="main w-full h-screen flex justify-center items-center bg-gray-100">
-      <div className="container max-w-xl border shadow-xl p-5 rounded-md  bg-white">
+      <div className="container max-w-xl border shadow-xl p-5 rounded-md  md:bg-whitebg-red-400">
         <Header />
         <Content/>
       </div>
